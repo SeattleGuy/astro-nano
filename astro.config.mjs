0b8be6fe43ffe3b40923b1d9d14de24bbae23edf
SeattleGuy <me@jcvieth.com>
2025-10-03 21:39:44 -0700

    Installed Astro Matomo
    https://github.com/felix-berlin/astro-matomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import matomo from 'astro-matomo';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(), 
+    sitemap(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://bits.jcvieth.com/",
+      setCookieDomain: "*.jcvieth.com",
+      trackerUrl: "js/", // defaults to matomo.php
+      srcUrl: "js/", // defaults to matomo.js
+      siteId: 1,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+      viewTransition: {
+        contentElement: "main"
+      }
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
